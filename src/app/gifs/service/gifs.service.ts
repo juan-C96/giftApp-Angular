9768fc108c5f136f5443eff4c4ea010ba886eb59
Juan Carlos Hernandez <jchernandez9606@gmail.com>
2022-07-19 12:19:33 +0200

    historial persistente

--- src/app/gifs/service/gifs.service.ts
+++ src/app/gifs/service/gifs.service.ts
@@ -16,7 +16,14 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor( private http: HttpClient ){}
+  constructor( private http: HttpClient ){
+
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    /* if( localStorage.getItem( 'historial' ) ){
+      this._historial = JSON.parse( localStorage.getItem( 'historial' )! );
+    } */
+
+  }
 
   buscarGifs(query: string) {
 
@@ -26,6 +33,8 @@ export class GifsService {
 
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
 
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=X5nSp2RqIxQBpQK8RkTtHfFZVSIaVLMh&q=${ query }&limit=10`)
